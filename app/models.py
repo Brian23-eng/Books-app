dad05b237606d529867b816d5f026d310d4b52fb
Alex Kiprop <kipropalex59@gmail.com>
2019-10-29 13:04:58 +0300

    create users class

--- app/models.py
+++ app/models.py
@@ -22,32 +22,8 @@ class User(UserMixin,db.Model):
 
     password_hash = db.Column(db.String(255))
 
-    photos = db.relationship('PhotoProfile',backref = 'user',lazy = "dynamic")
+   
 
-    reviews = db.relationship('Review',backref = 'user',lazy = "dynamic")
+    def __repr__(self):
 
-    # @property
-
-    # def password(self):
-
-    #     raise AttributeError('You cannnot read the password attribute')
-
-    # @password.setter
-
-    # def password(self, password):
-
-    #     self.password_hash = generate_password_hash(password)
-
-    # def verify_password(self,password):
-
-    #     return check_password_hash(self.password_hash,password)
-
-    # def save_user(self):
-
-    #     db.session.add(self)
-
-    #     db.session.commit()
-
-    # def __repr__(self):
-
-    #     return f'User {self.username}'
+        return f'User {self.username}'
