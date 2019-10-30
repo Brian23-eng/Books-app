c0ac99b17eaeca40d385e67557a2eb42fbaaffdc
brian23-eng <b.odhiambo.bo@gmail.com>
2019-10-30 10:00:23 +0300

    add api-key with book-base-url

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ class Config:
     '''
     General configuration parent class
     '''
-
+    
+    BOOK_API_BASE_URL = 'https://www.goodreads.com/search?key={}&q={}'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://sam:1234@localhost/book'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SECRET_KEY = os.environ.get('SECRET_KEY')
